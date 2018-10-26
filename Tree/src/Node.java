f458375bc6441d77486bcbb4cc4382b1aa6d23fa
lesslate <39299919+lesslate@users.noreply.github.com>
2018-10-27 01:38:25 +0900

    Initial commit

---
+++ Tree/src/Node.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Node {
+	public int iData;
+	public double dData;
+	public Node leftChild;
+	public Node rightChild;
+
+}
+
+
---
+++ Tree/src/Tree.java
@@ -0,0 +1,91 @@
+
+class Tree
+{
+	private Node root;
+
+	public Tree()
+	{root = null;}
+
+	public void insert(int id,double dd)
+	{
+		Node newNode = new Node();
+		newNode.iData = id;
+		newNode.dData = dd;
+		if(root==null)
+			root = newNode;
+		else
+		{
+			Node current = root;
+			Node parent;
+			while(true)
+			{
+				parent = current;
+				if(id<current.iData)
+				{
+					current = current.leftChild;
+					if(current==null)
+					{
+						parent.leftChild = newNode;
+						return;
+					}
+				}
+			
+			else
+			{
+				current = current.rightChild;
+				if(current == null) {
+					parent.rightChild= newNode;
+				return;
+			}
+				}
+			}
+		}
+	}
+
+	public void traverse()
+	{
+		System.out.print("\nPreorder traversal: ");	
+		preOrder(root);
+		System.out.print("\ninOrder traversal: ");	
+		inOrder(root);
+		System.out.print("\nPostOrder traversal: ");	
+		postOrder(root);
+	    System.out.println();
+	}
+	
+
+private void preOrder(Node localRoot)
+{
+	if(localRoot != null)
+	{
+		System.out.print(localRoot.iData+" ");
+		preOrder(localRoot.leftChild);
+		preOrder(localRoot.rightChild);
+	}
+}
+
+private void inOrder(Node localRoot)
+{
+	if(localRoot != null)
+	{
+		
+		inOrder(localRoot.leftChild);
+		System.out.print(localRoot.iData + " ");
+		inOrder(localRoot.rightChild);
+		
+	}
+}
+private void postOrder(Node localRoot)
+{
+	if(localRoot != null)
+	{
+		
+		postOrder(localRoot.leftChild);
+		
+		postOrder(localRoot.rightChild);
+		System.out.print(localRoot.iData + " ");
+		
+	}
+}
+
+}
